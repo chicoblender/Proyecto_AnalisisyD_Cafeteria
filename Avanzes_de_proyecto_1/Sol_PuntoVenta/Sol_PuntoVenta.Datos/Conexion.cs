20264c407f2004accc647fc17e214699e5c671ae
chicoblender <cimble9@gmail.com>
2023-09-29 17:35:41 -0400

    Se agrga la carpeta Avanzes que contiene lo avanzado

---
+++ Avanzes_de_proyecto_1/Sol_PuntoVenta/Sol_PuntoVenta.Datos/Conexion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+namespace Sol_PuntoVenta.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static Conexion Con = null;
+        private Conexion() 
+        {
+            this.Base = "";
+            this.Servidor = "";
+            this.Usuario= "";
+            this.Clave = "";
+
+
+        } 
+        public SqlConnection CrearConexion() 
+        {
+            SqlConnection Cadena = new SqlConnection();
+            try 
+            {
+                Cadena.ConnectionString = "Server" + this.Servidor +
+                      "; Database=" + this.Base +
+                      "; User Id=" + this.Usuario +
+                      "; Password=" + this.Clave;
+
+
+            }
+            catch (Exception ex) 
+            {
+                Cadena = null;
+                throw ex;
+            }
+            return Cadena;
+        }
+
+        public static Conexion getInstancia() 
+        {
+            if(Con==null)
+            {
+                Con= new Conexion();
+            }
+            return Con;
+        }
+    }
+}
